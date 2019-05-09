649bb98b5959548496371c8f1b776aca97d6f63e
José Carlos <30965492+jcval94@users.noreply.github.com>
2019-05-09 15:56:45 +0100

    Create error.R
    4 funciones de error, entre más cerca del 0, más parecidas son.

---
+++ error.R
@@ -0,0 +1,17 @@
+#Diferentes tipos de error, queda pendiente para que el usuario elija unar los quantiles o no
+error<-function(X,MA){
+  qqX<-quantile(X,seq(0,100,by=1)/100)
+  qqMA<-quantile(MA,seq(0,100,by=1)/100)
+  #0 es mayor igualdad
+  U<-sqrt(sum((qqX-qqMA)^2)/length(qqX))/(sqrt(sum(qqX^2)/length(qqX))+sqrt(sum(qqMA^2)/length(qqMA)))
+  #0 es mayor igualdad en medias
+  Um<-(mean(qqX)-mean(qqMA))/(sqrt(sum(qqX^2)/length(qqX))+sqrt(sum(qqMA^2)/length(qqMA)))
+  #Nivel de correlación
+
+  Us<-(sd(qqX)/sqrt(length(qqX))-sd(qqMA)/sqrt(length(qqMA)))/(sqrt(sum(qqX^2)/length(qqX))+sqrt(sum(qqMA^2)/length(qqMA)))
+
+  #Nivel de correlaci?n
+  Cor<-1-cor(qqMA,qqX)
+
+  return(c(U,Um,Us,Cor))
+}
