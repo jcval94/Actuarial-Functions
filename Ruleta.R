b3f1c59b5858fffd3e30ef15b793084e732fe1ff
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-05-23 21:54:08 +0100

    Update Ruleta.R
    Ejemplos de ganancias

--- Ruleta.R
+++ Ruleta.R
@@ -17,3 +17,47 @@ ruleta<-function(apuestas=1,posiciones=3){
     return(0)
   }
 }
+
+#Hagamos 1000 apuestas de 1$ a la mitad de la ruleta
+Resultados<-c()
+Inversion<-c()
+for(i in 1:1000){
+  apuestas<-1
+  posiciones = c(1:18)
+  Inversion[i]<-apuestas*length(posiciones)
+  Resultados[i]<-ruleta(apuestas,posiciones)
+}
+
+Ganancia<-sum(Resultados)-sum(Inversion)
+print(Ganancia)
+
+########################################################################
+#Es claro que siempre pierdo, qué forma hay de ganar?
+#Cada vez que pierdo duplico *mi apuesta *el número de casillas
+
+Resultados<-c()
+Inversion<-c()
+Apuestas<-c()
+apuestas<-1
+for(i in 1:1000){
+  
+  posiciones = c(1:18)
+  
+  Inversion[i]<-apuestas*length(posiciones)
+  Resultados[i]<-ruleta(apuestas,posiciones)
+  if(Resultados[i]==0){
+    apuestas<-apuestas*2
+  }else{
+    apuestas<-1
+  }
+  Apuestas[i]<-apuestas
+}
+
+Ganancia<-sum(Resultados-Inversion)
+print(Ganancia)
+
+#Veamos algunos resultados:
+summary(Resultados)
+summary(Inversion)
+
+
