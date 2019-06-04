4b20998ff6bd6abb6d25ed13a4ababfcb6cf683c
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-06-05 00:43:15 +0100

    Update Modelo hip.R
    Nuevos planes

--- Modelo hip.R
+++ Modelo hip.R
@@ -1,3 +1,4 @@
+
 #Ejemplo de un modelo numérico generalizado para un N suf. grande de personas en el modelo
 
 #Tenemos 1000 personas dentro de nuestro modelo
@@ -19,24 +20,33 @@ I<-rpois(1000,63)
 #Para este caso es sencillo, pues es resta de Poissones
 Perm<-rpois(1000,75-63)
 
-#Buscamos también la distribución del costo promedio anual. Si el ejercicio se pasa a
+#Buscamos también la distribución del costo promedio Anual_P1. Si el ejercicio se pasa a
 #mensual, debemos reescribir la variable Perm en función de los meses, análogaente para semanas, etc.
 #Se trabajará con una dist. gamma debido a sus bonitas propiedades (también se puede
 #usar una Beta ajustada, pero en realidad se debe obtener de muestras reales)
+#Simularemos más escenarios para los distintos planes
 
-Anual<-rgamma(1000,30000,.1)
+Anual_P1<-rgamma(1000,30000,.1)
+
+Anual_P2<-rgamma(1000,20000,.1)
+
+Anual_P3<-rgamma(1000,10000,.1)
+
+#Veamos la gráfica de los tres planes
+
+plot(density(c(Anual_P1,Anual_P2,Anual_P3)))
 
 #####################################
 #Siendo este el caso, veamos los gastos que implica esta operación:
-Costo<-Perm*Anual
+Costo<-Perm*Anual_P1
 #Veamos un pequeño resumen
 summary(Costo)
 #De ser este el caso, el costo de las viviendas deberían rondar los 3,6M
 #####################################
 
-#Suponemos que la tasa de crecimiento del costo de la vivienda es X% anual
-#Suponemos que el costo de los medicamentos/equipo/salario de quien los atiende, etc crece X% anual
-#Y suponemos que nuestra población aumenta su edad de supervivencia con una tasa X% anual
+#Suponemos que la tasa de crecimiento del costo de la vivienda es X% Anual_P1
+#Suponemos que el costo de los medicamentos/equipo/salario de quien los atiende, etc crece X% Anual_P1
+#Y suponemos que nuestra población aumenta su edad de supervivencia con una tasa X% Anual_P1
 
 #Digamos que se distribuye como una Normal, solo para ejemplificar.
 #Estos números pueden ser obtenidos con modelos predictivos, no necesariamente con
@@ -53,16 +63,16 @@ Pond_Fin<-Pond_Vivienda*Pond_Servicio*Pond_Superv
 #Siendo así, ya tenemos nuestro modelo listo
 
 #Primero veamos cuánto costará la persona 1.
-Costo1<-sum(Anual[1]*Pond_Fin[1:Perm[1]])
+Costo1<-sum(Anual_P1[1]*Pond_Fin[1:Perm[1]])
 
 #Repliquemos para nuestras 1000 personas
-Costo<-purrr::map_dbl(1:1000,~sum(Anual[.x]*Pond_Fin[1:Perm[.x]]))
+Costo<-purrr::map_dbl(1:1000,~sum(Anual_P1[.x]*Pond_Fin[1:Perm[.x]]))
 Costos<-data.frame(Persona=1:1000,Perm=Perm,Costo=Costo)
 #Para sacar el valor presente, debemos dividir entre el Pond_Vivienda por cada año
 #Para el primer cliente:
-Valor_Presente1<-sum(Anual[1]*(Pond_Fin/Pond_Vivienda)[1:Perm[1]])
+Valor_Presente1<-sum(Anual_P1[1]*(Pond_Fin/Pond_Vivienda)[1:Perm[1]])
 
-VP_Costos<-purrr::map_dbl(Perm,~sum(Anual[.x]*(Pond_Fin/Pond_Vivienda)[1:Perm[.x]]))
+VP_Costos<-purrr::map_dbl(Perm,~sum(Anual_P1[.x]*(Pond_Fin/Pond_Vivienda)[1:Perm[.x]]))
 
 Costos<-cbind(Costos,data.frame(Valor_Presente_Costos=VP_Costos))
 #Sabemos que el costo de una vivienda es de 1M en promedio
