ce132478fcfd57d81497ccf399fb41d76f27221f
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-06-18 00:07:24 +0100

    Create Abstract.R
    Documento descriptivo para proyecto AMA

---
+++ Abstract.R
@@ -0,0 +1,16 @@
+Este artículo se presenta como una respuesta a las principales problemáticas que enfrentan las personas de la tercera edad en la república mexicana, tales como la soledad, el abandono, la poca prevención de la enfermedad y más aún, la pérdida de la independencia, involucrando a las instituciones bancarias y de seguros para combatir dichos impedimentos a través de un producto sustentable y permisible para una gran parte de esta población.
+
+Se dará un breve marco socioeconómico sobre la mortalidad de las personas de la tercera edad y se verá una proyección de ésta para el año 2050, se hablará de la calidad de vida y un desglose de los problemas que se relacionan con la vejez.
+
+Se explicará a profundidad el funcionamiento de este producto, desde los requisitos para ser acreedor de éste, los beneficios ofrecidos, el aumento o disminución de su cobertura, su modificación y cancelación.
+
+Se expondrán las circunstancias concretas para el completo entendimiento desde la perspectiva del cliente y de la compañía, mostrando los alcances económicos en donde intersectarán ambas posturas.
+
+Se darán detalles técnicos, una breve introducción al esquema legal y con mayor profundidad se tocará la rentabilidad, la teoría estadística, computacional y actuarial que lo respaldan y en paralelo, medidas de monitoreo que garanticen su estabilidad y sano desarrollo.
+
+El impacto social, la sustentabilidad en el tiempo e importancia de las buenas prácticas serán revisadas, así como las coberturas, planes y beneficios futuros junto con una motivación para la permanencia y procesos de prevención por fallo del producto.
+Dada la importancia de la selección correcta del cliente, será esquematizado el proceso de toma de decisión para cada una de las figuras involucradas, haciendo un símil con el algoritmo de bosques aleatorios.
+
+Aunado a ello, se mencionará los cambios que deberán enfrentar las instituciones bancarias, de seguros o ligadas con sus actividades para integrar paulatinamente este modelo solución, tanto en el producto como en el esquema de decisión.
+Nos volcaremos sobre temas de mejora del servicio, medición de la calidad y hablaremos de la ética y responsabilidad social.
+Se mostrará una postura sobre las barreas físicas o ideológicas que podrían permear, dando respuesta a cada punto (uso de la tecnología, discapacidad, inclusión de gente que sepa lenguaje de señas)
